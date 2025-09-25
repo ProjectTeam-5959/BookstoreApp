5296cb8f5f6a50a487a45392ad1022880d3525de
jinhee kim <lsa68803@naver.com>
2025-09-25 20:03:28 +0900

    [library] feat: Library 엔티티에 연관관계 매핑 추가

--- src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
+++ src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.bookstoreapp.domain.user.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -15,9 +19,6 @@ public class Library {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /*
-    연결 후 주석 풀기
-
     // 유저 1명당 서재 1개
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
@@ -28,7 +29,7 @@ public class Library {
     // mappedBy = "library" : 양방향 매핑에서 외래키(FK) 관리하는 주인이 누구인지 지정
     // cascade = CascadeType.ALL : 라이브러리 저장/삭제 시, 그 안에 속한 LibraryBook도 같이 저장/삭제됨
     // orphanRemoval = true : 부모 컬렉션에서 자식 엔티티 제거 시, DB도 자동 삭제
-    private List<LibraryBook> libraryBooks = new ArrayList<>();
+    private List<LibraryBooks> libraryBooks = new ArrayList<>();
 
     public Library(User user) {
         this.user = user;
@@ -37,7 +38,4 @@ public class Library {
      public static Library of(User user) {
         return new Library(user);
     }
-
-     */
-
 }
