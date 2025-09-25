03b15577f95f1ef3abb2ccafe8de7cb0f3839f2e
hailee2 <slowslow9150@gmail.com>
2025-09-25 20:42:17 +0900

    [auth] feat : SpringSecurity+JWT 설정,필터,토큰생성 추가

---
+++ src/main/java/org/example/bookstoreapp/domain/auth/dto/AuthUser.java
@@ -0,0 +1,24 @@
+package org.example.bookstoreapp.domain.auth.dto;
+
+import lombok.Getter;
+import org.example.bookstoreapp.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class AuthUser {
+
+    private final Long id;
+    private final String email;
+    private final Collection<? extends GrantedAuthority> authorities;
+//    private final String nickname;    토큰에 nickname 존재하지만 AuthUser에 닉네임이 꼭 들어가야하는건 아님!
+
+    public AuthUser(Long id, String email, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.authorities = List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+}
