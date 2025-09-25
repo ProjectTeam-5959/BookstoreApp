2262b28c7c8aea995f9aa9e639a21d2c76c0be3e
jinhee kim <lsa68803@naver.com>
2025-09-26 01:54:25 +0900

    [library] feat: LibraryBookResponse 구현

---
+++ src/main/java/org/example/bookstoreapp/domain/library/dto/response/LibraryBookResponse.java
@@ -0,0 +1,20 @@
+package org.example.bookstoreapp.domain.library.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class LibraryBookResponse {
+
+    private Long bookId;
+    private String title;
+    private List<String> authors;
+    private String publisher;
+    private String isbn;
+    private LocalDateTime publicationDate;
+    private LocalDateTime addedAt;
+}
