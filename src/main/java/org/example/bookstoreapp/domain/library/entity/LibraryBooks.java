408db1d4b534779828b94baf7add0e9664057308
jinhee kim <lsa68803@naver.com>
2025-09-25 20:02:50 +0900

    [library] feat: LibraryBooks 엔티티에 복합 유니크 및 연관관계 매핑 추가

--- src/main/java/org/example/bookstoreapp/domain/library/entity/LibraryBooks.java
+++ src/main/java/org/example/bookstoreapp/domain/library/entity/LibraryBooks.java
@@ -4,26 +4,24 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.bookstoreapp.domain.book.entity.Book;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-/*
-연결 후 주석풀기
-
 // 컬럼자체에 unique = true  할 경우, 한 서재에는 책이 단 1권 밖에 못들어감
 // 그러므로 FK 컬럼 단위로 unique 걸 수 없다.
-// 복합 유니크 키!
+// 복합 유니크 키 사용!
 // 한 서재에 같은 책이 중복 되지 않도록 막음
 // (library_id, book_id) 쌍이 유일해야 함 -> 테이블 레벨에서 설정
 @Table(
         name = "librarybooks",
         uniqueConstraints = {
-                @UniqueConstraint(ColumnNames = {"library_id", "book_id"})
+                @UniqueConstraint(columnNames = {"library_id", "book_id"})
       }
-)*/
+)
 public class LibraryBooks {
 
     @Id
@@ -35,21 +33,17 @@ public class LibraryBooks {
     @JoinColumn(name = "library_id", nullable = false)
     private Library library;
 
-    /*
-    연결 후 주석풀기
 
     // 책 (FK)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
-     */
+
 
     // 서재에 책 등록 시간
     @Column(nullable = false)
     private LocalDateTime addedAt;
 
-    /*
-    연결 후 주석풀기
     protected LibraryBooks(Library library, Book book) {
         this.library = library;
         this.book = book;
@@ -60,5 +54,4 @@ public class LibraryBooks {
         return new LibraryBooks(library, book);
     }
 
-     */
 }
