d524740c9ea5e7c28807ad434ec3af39d6801664
jinhee kim <lsa68803@naver.com>
2025-09-26 09:55:58 +0900

    [library] refactor: LibraryBookResponse 의 publicationDate 타입 변경

--- src/main/java/org/example/bookstoreapp/domain/library/dto/response/LibraryBookResponse.java
+++ src/main/java/org/example/bookstoreapp/domain/library/dto/response/LibraryBookResponse.java
@@ -3,6 +3,7 @@ package org.example.bookstoreapp.domain.library.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,6 +16,6 @@ public class LibraryBookResponse {
     private List<String> authors;
     private String publisher;
     private String isbn;
-    private LocalDateTime publicationDate;
+    private LocalDate publicationDate;
     private LocalDateTime addedAt;
 }
