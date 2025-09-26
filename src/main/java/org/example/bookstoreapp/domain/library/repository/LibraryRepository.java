be4623ef00348e0e149ce5b8e404ebbb50e3d668
jinhee kim <lsa68803@naver.com>
2025-09-26 19:12:29 +0900

    [library] feat: 내 서재 조회 및 생성(최초 1회) 구현

--- src/main/java/org/example/bookstoreapp/domain/library/repository/LibraryRepository.java
+++ src/main/java/org/example/bookstoreapp/domain/library/repository/LibraryRepository.java
@@ -3,5 +3,9 @@ package org.example.bookstoreapp.domain.library.repository;
 import org.example.bookstoreapp.domain.library.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LibraryRepository extends JpaRepository<Library, Long> {
+
+    Optional<Library> findByUserId(Long userId);
 }
---
+++ src/main/java/org/example/bookstoreapp/domain/library/service/LibraryService.java
@@ -0,0 +1,66 @@
+package org.example.bookstoreapp.domain.library.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bookstoreapp.domain.auth.dto.AuthUser;
+import org.example.bookstoreapp.domain.book.entity.Book;
+import org.example.bookstoreapp.domain.library.dto.response.LibraryBookResponse;
+import org.example.bookstoreapp.domain.library.dto.response.LibraryResponse;
+import org.example.bookstoreapp.domain.library.entity.Library;
+import org.example.bookstoreapp.domain.library.repository.LibraryRepository;
+import org.example.bookstoreapp.domain.user.entity.User;
+import org.example.bookstoreapp.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LibraryService {
+
+    private final LibraryRepository libraryRepository;
+    private final UserRepository userRepository;
+
+    // 내 서재 조회 //
+    // 1회 서재 생성 로직 포함되므로 readOnly = true 불가
+    public LibraryResponse getMyLibrary(AuthUser authUser) {
+
+        // 내 서재 가져오기 + 서재 생성(최초 1회만)
+        // findByUserId -> Optional! => 있으면 Library 반환 / 없으면 orElseGet 구문 실행되어 서재 생성
+        // orElseGet : 값이 비어 있을 때만 실행되는 대체 로직
+        Library library = libraryRepository.findByUserId(authUser.getId()).orElseGet(
+                () -> {
+                    User user = userRepository.findById(authUser.getId()).orElseThrow(
+                            () -> new IllegalArgumentException("해당하는 유저가 없습니다.")
+                    );
+                    return libraryRepository.save(Library.of(user));
+                }
+        );
+
+        // 서재에 담긴 책들 반환
+        List<LibraryBookResponse> bookResponses = library.getLibraryBooks().stream()
+                .map(libraryBooks -> {
+                    Book book = libraryBooks.getBook();
+
+                    // 작가 리스트 가져오기
+                    List<String> authors = book.getBookContributors().stream()
+                            .map(bookContributor -> bookContributor.getContributor().getName())
+                            .toList();
+
+                    // 책 + 작가 정보 반환
+                    return new LibraryBookResponse(
+                            book.getId(),
+                            book.getTitle(),
+                            authors,
+                            book.getPublisher(),
+                            book.getIsbn(),
+                            book.getPublicationDate(),
+                            libraryBooks.getAddedAt()
+                    );
+                })
+                .toList();
+
+        return new LibraryResponse(bookResponses);
+    }
+}
