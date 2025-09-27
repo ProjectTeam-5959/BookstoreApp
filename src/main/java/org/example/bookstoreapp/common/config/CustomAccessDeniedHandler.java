a8ef297fd9b75f21de412e7d15094a835d7af808
hailee2 <slowslow9150@gmail.com>
2025-09-27 16:36:29 +0900

    [Auth] feat : 접근 거부 이벤트 발생 시 디버깅을 위한 로그 추가

--- src/main/java/org/example/bookstoreapp/common/config/CustomAccessDeniedHandler.java
+++ src/main/java/org/example/bookstoreapp/common/config/CustomAccessDeniedHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.example.bookstoreapp.common.response.ApiResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private final ObjectMapper objectMapper;
@@ -21,6 +23,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.warn("접근이 거부되었습니다. URI: {}, 이유: {}", request.getRequestURI(), accessDeniedException.getMessage());
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json;charset=UTF-8");
 
