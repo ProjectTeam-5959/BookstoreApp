71b624631d0e20c3dfa96acafa2d3d58ef7b54c5
hailee2 <slowslow9150@gmail.com>
2025-09-27 17:28:29 +0900

    [Auth/User] clean : 테스트 목록 생성

--- src/test/java/org/example/bookstoreapp/domain/auth/AuthServiceTest.java
+++ src/test/java/org/example/bookstoreapp/domain/auth/AuthServiceTest.java
@@ -1,5 +1,40 @@
 package org.example.bookstoreapp.domain.auth;
 
+import org.example.bookstoreapp.domain.auth.service.AuthService;
+import org.example.bookstoreapp.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private AuthService userService;
+
+    @Test
+    void signup_성공한다() {}
+
+    @Test
+    void signup_이메일중복이면_DUPLICATE_EMAIL_예외() {}
+
+    @Test
+    void signup_닉네임중복이면_DUPLICATE_NICKNAME_예외() {}
+
+    @Test
+    void login_성공한다() {}
+
+    @Test
+    void login_이메일없으면_USER_NOT_FOUND_BY_EMAIL_예외() {}
+
+    @Test
+    void login_비밀번호틀리면_PASSWORD_MISMATCH_예외() {}
+
+    @Test
+    void withdraw_성공한다() {}
 
 }
