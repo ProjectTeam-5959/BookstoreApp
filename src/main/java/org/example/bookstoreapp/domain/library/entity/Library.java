29ff4841567f575cc302356b9d38cab6228e0919
jinhee kim <lsa68803@naver.com>
2025-09-28 02:11:32 +0900

    [library] feat: 내 서재에 책 추가 구현

--- src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
+++ src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
@@ -25,17 +25,24 @@ public class Library {
     private User user;
 
     // 서재에 담긴 책들 (중간 테이블)
-    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
     // mappedBy = "library" : 양방향 매핑에서 외래키(FK) 관리하는 주인이 누구인지 지정
     // cascade = CascadeType.ALL : 라이브러리 저장/삭제 시, 그 안에 속한 LibraryBook도 같이 저장/삭제됨
-    // orphanRemoval = true : 부모 컬렉션에서 자식 엔티티 제거 시, DB도 자동 삭제
     private List<LibraryBook> libraryBooks = new ArrayList<>();
 
     public Library(User user) {
         this.user = user;
     }
 
-     public static Library of(User user) {
+    // 정적 팩토리 메서드(엔티티 생성용)
+    public static Library of(User user) {
         return new Library(user);
     }
+
+    // 편의 메서드 : 내 서재에 책 추가
+    public void addBook(LibraryBook libraryBook) {
+        // 부모(library) 컬렉션에 추가
+        libraryBooks.add(libraryBook);
+    }
 }
+
