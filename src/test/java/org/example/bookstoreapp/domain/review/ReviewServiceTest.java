8888d4f11bf533353b57b9a4a3b025b79491944c
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-28 19:57:31 +0900

    [Review] test : 리뷰 조회 성공 테스트 - 무한스크롤 페이징 구현

--- src/test/java/org/example/bookstoreapp/domain/review/ReviewServiceTest.java
+++ src/test/java/org/example/bookstoreapp/domain/review/ReviewServiceTest.java
@@ -18,14 +18,20 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.example.bookstoreapp.domain.book.entity.BookCategory.NOVEL;
 import static org.example.bookstoreapp.domain.user.enums.UserRole.ROLE_ADMIN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -87,5 +93,51 @@ public class ReviewServiceTest {
         assertEquals(book, captor.getValue().getBook());
     }
 
+    @Test
+    @DisplayName("리뷰 조회 성공 테스트 - 무한스크롤 페이징")
+    void getReviews_success_infiniteScroll() {
+        // given
+        org.springframework.data.domain.Pageable pageable = Pageable.ofSize(10); // 하나의 페이지에 10개 씩 출력할 수 있다.
+        List<Review> reviews = new ArrayList<>(); // 테스트용 리뷰 데이터를 담을 리스트
+
+        // 데이터 10개 생성
+        for (int i = 0; i < 11; i++) {
+            Review review = Review.builder()
+                    .id((long) i)
+                    .content((i + 1) + "번 째 리뷰")
+                    .user(user)
+                    .book(book)
+                    .build();
+            reviews.add(review); // 생성할 때마다 리스트에 추가
+        }
+
+        /**
+         참고! Slice는 내부적으로 limit(pageSize + 1) 로 쿼리를 날린다.
+         예를 들어 pageSize=10이면 실제 SQL은 limit 11 로 조회
+         */
+        // SliceImpl의 첫 번째 인자는 실제 데이터 리스트, 두 번째 인자는 페이징 정보, 세 번째 인자는 다음 페이지가 존재하는 지 여부 확인 true면 다음 페이지가 존재한다는 것을 의미함.
+        Slice<Review> reviewSlice = new SliceImpl<>(
+                reviews.subList(0, 10), // 반환 값 10개로 설정
+                pageable,
+                reviews.size() > 10 // 데이터가 11개 이상이라면 hasNext가 true -> 다음 페이지가 존재한다.
+        );
+
+        // reviewRepository가 호출되면 만들어 둔 reviewSlice를 반환
+        when(reviewRepository.findByUserId(authUser.getId(), pageable)).thenReturn(reviewSlice);
+
+        // when
+        // 실제 서비스 메서드를 호출, Review 에서 ReviewResponse로 변환된 결과를 반환
+        Slice<ReviewResponse> result = reviewService.getReviews(authUser, pageable);
+
+        // then
+        //조회된 리뷰가 10개 인가?
+        assertEquals(10, result.getContent().size());
+
+        // 다음 페이지가 존재함을 확인
+        assertTrue(result.hasNext());
+
+        // 첫 번째 리뷰의 내용이 "1번 째 리뷰"라고 잘 전달 되었는 지 확인!
+        assertEquals("1번 째 리뷰", result.getContent().get(0).getContent());
+    }
 
 }
