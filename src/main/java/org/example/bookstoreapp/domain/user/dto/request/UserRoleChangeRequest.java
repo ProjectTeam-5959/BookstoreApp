7a182d469bb378998580bdafb2deddd4640a8abe
hailee2 <slowslow9150@gmail.com>
2025-09-28 17:22:47 +0900

    [user] userAdminServiceTest 테스트 코드 작성

--- src/main/java/org/example/bookstoreapp/domain/user/dto/request/UserRoleChangeRequest.java
+++ src/main/java/org/example/bookstoreapp/domain/user/dto/request/UserRoleChangeRequest.java
@@ -1,9 +1,11 @@
 package org.example.bookstoreapp.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.bookstoreapp.domain.user.enums.UserRole;
 
 @Getter
+@AllArgsConstructor
 public class UserRoleChangeRequest {
     private UserRole role;
 }
