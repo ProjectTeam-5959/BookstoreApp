5129a622d8520d5c614e4252fc644db4d97bdfde
jinhee kim <lsa68803@naver.com>
2025-09-29 17:51:09 +0900

    [library] fix: Library 엔티티에 cascade 복원

--- src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
+++ src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
@@ -25,9 +25,10 @@ public class Library {
     private User user;
 
     // 서재에 담긴 책들 (중간 테이블)
-    @OneToMany(mappedBy = "library")
+    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
     // mappedBy = "library" : 양방향 매핑에서 외래키(FK) 관리하는 주인이 누구인지 지정
     // cascade = CascadeType.ALL : 라이브러리 저장/삭제 시, 그 안에 속한 LibraryBook도 같이 저장/삭제됨
+    // cascade = CascadeType.ALL 삭제 시 중간 테이블에 저장/삭제 반영 안됨
     private List<LibraryBook> libraryBooks = new ArrayList<>();
 
     public Library(User user) {
