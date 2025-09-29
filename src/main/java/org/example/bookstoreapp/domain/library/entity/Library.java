7038b6191a060e3a6b13989d65046d8620e3e823
wlslwlsl1108 <lsa68803@naver.com>
2025-09-29 09:38:45 +0900

    [library] refactor : 불필요한 cascade 삭제

--- src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
+++ src/main/java/org/example/bookstoreapp/domain/library/entity/Library.java
@@ -25,7 +25,7 @@ public class Library {
     private User user;
 
     // 서재에 담긴 책들 (중간 테이블)
-    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "library")
     // mappedBy = "library" : 양방향 매핑에서 외래키(FK) 관리하는 주인이 누구인지 지정
     // cascade = CascadeType.ALL : 라이브러리 저장/삭제 시, 그 안에 속한 LibraryBook도 같이 저장/삭제됨
     private List<LibraryBook> libraryBooks = new ArrayList<>();
