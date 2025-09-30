46cb9f097c18d556f7618a8363524eb8678ee4c6
jinhee kim <lsa68803@naver.com>
2025-09-30 14:39:00 +0900

    [library] feat: 서재 조회 시 Slice 기반 페이징 추가

--- src/main/java/org/example/bookstoreapp/domain/library/dto/response/LibraryBookResponse.java
+++ src/main/java/org/example/bookstoreapp/domain/library/dto/response/LibraryBookResponse.java
@@ -2,9 +2,13 @@ package org.example.bookstoreapp.domain.library.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.example.bookstoreapp.domain.book.entity.Book;
+import org.example.bookstoreapp.domain.contributor.entity.ContributorRole;
+import org.example.bookstoreapp.domain.library.entity.LibraryBook;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -14,4 +18,22 @@ public class LibraryBookResponse {
     private String title;
     private List<String> authors;
     private LocalDateTime addedAt;
+
+    // 정적 팩토리 메서드
+    public static LibraryBookResponse from(LibraryBook libraryBook) {
+
+        Book book = libraryBook.getBook();
+
+        List<String> authors = book.getBookContributors().stream()
+                .filter(bookContributor -> bookContributor.getRole() == ContributorRole.AUTHOR)
+                .map(bookContributor -> bookContributor.getContributor().getName())
+                .collect(Collectors.toList());
+
+        return new LibraryBookResponse(
+                book.getId(),
+                book.getTitle(),
+                authors,
+                libraryBook.getAddedAt()
+        );
+    }
 }
