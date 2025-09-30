ebaae3a9a9158bc66605c87a8172bec05ce55e6a
hailee2 <slowslow9150@gmail.com>
2025-09-30 14:49:10 +0900

    [auth] feat: 인증인가 통합 테스트

--- src/main/java/org/example/bookstoreapp/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/bookstoreapp/domain/user/controller/UserAdminController.java
@@ -1,18 +1,19 @@
 package org.example.bookstoreapp.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.bookstoreapp.common.response.ApiResponse;
+import org.example.bookstoreapp.domain.auth.dto.AuthUser;
 import org.example.bookstoreapp.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.bookstoreapp.domain.user.dto.response.UserResponse;
 import org.example.bookstoreapp.domain.user.enums.UserRole;
 import org.example.bookstoreapp.domain.user.service.UserAdminService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Secured(UserRole.Authority.ADMIN)
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +26,13 @@ public class UserAdminController {
         return ResponseEntity.ok(
                 ApiResponse.success("권한을 변경했습니다.",userAdminService.changeUserRole(userId,request))
         );
+    }
+
+
+    @GetMapping("/admin/test")
+    public void test(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("User ID: {}", authUser.getId());
+        log.info("Email: {}", authUser.getEmail());
+        log.info("Authorities: {}", authUser.getAuthorities());
     }
 }
