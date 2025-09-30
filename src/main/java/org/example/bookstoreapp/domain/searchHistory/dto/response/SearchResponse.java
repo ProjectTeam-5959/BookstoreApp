c3c3e4e68ab4977b3da4531a8b9cafd02e88b708
cjn404 <moi404@naver.com>
2025-10-01 01:24:56 +0900

    [searchhistory] feat: SearchResponse내 정적 팩토리 메서드 추가

--- src/main/java/org/example/bookstoreapp/domain/searchHistory/dto/response/SearchResponse.java
+++ src/main/java/org/example/bookstoreapp/domain/searchHistory/dto/response/SearchResponse.java
@@ -1,10 +1,13 @@
 package org.example.bookstoreapp.domain.searchHistory.dto.response;
 
 import lombok.Getter;
+import org.example.bookstoreapp.domain.book.entity.Book;
 import org.example.bookstoreapp.domain.book.entity.BookCategory;
+import org.example.bookstoreapp.domain.bookcontributor.entity.BookContributor;
 import org.example.bookstoreapp.domain.contributor.dto.SearchContributorResponse;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -28,5 +31,19 @@ public class SearchResponse {
         this.contributors = contributors;
         this.category = category;
         this.createdAt = createdAt;
+    }
+
+    public static SearchResponse from(Book book) {
+        List<SearchContributorResponse> contributors = new ArrayList<>();
+        for (BookContributor bc : book.getBookContributors()) {
+            contributors.add(SearchContributorResponse.from(bc.getContributor(), bc.getRole()));
+        }
+        return new SearchResponse(
+                book.getId(),
+                book.getTitle(),
+                contributors,
+                book.getCategory(),
+                book.getCreatedAt()
+        );
     }
 }
