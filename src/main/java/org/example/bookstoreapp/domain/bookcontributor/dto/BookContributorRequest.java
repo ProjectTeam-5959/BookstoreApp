139cc1e8063f83d2a98c9ae78eaf71ad3024b295
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-01 01:12:50 +0900

    [Book] del : 불필요한 어노테이션 및 주석 제거

--- src/main/java/org/example/bookstoreapp/domain/bookcontributor/dto/BookContributorRequest.java
+++ src/main/java/org/example/bookstoreapp/domain/bookcontributor/dto/BookContributorRequest.java
@@ -1,13 +1,10 @@
 package org.example.bookstoreapp.domain.bookcontributor.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.example.bookstoreapp.domain.contributor.entity.ContributorRole;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookContributorRequest {
 
     @NotNull
